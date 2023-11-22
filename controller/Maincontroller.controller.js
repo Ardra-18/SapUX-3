ffb14a9b6fe75262af1690744e7430b98518b5b6
Ardra S M <84071302+Ardra-18@users.noreply.github.com>
2023-11-22 15:28:49 +0530

    Add files via upload

---
+++ controller/Maincontroller.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define(['sap/ui/core/mvc/Controller'],function(oController){
+    return oController.extend('com.simpleapp.Controller.Maincontroller',{
+        xmlfun:function(){
+            gv=this.getView()
+            var va=gv.byId('vertically')
+            va.destroyContent()
+        },
+        xmlfun1:function(){
+            gv=this.getView()
+            var va=gv.byId('vertically')
+            console.log('d');
+            va.addContent('<text text="this is added text"/>')
+        }
+    })
+})
+
